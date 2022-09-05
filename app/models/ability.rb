f04370b9689ff98ea7f08109529fa409dfa75ee5
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-09-05 11:16:41 +0100

    Added Ability Model for Authorization

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    cannot :read, :all
+
+    return unless user.present?
+
+    can :create, :all
+    can :destroy, :all, User_id: user.id
+  end
+end
