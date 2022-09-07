69fe7d83c67bff2d3c78212e71017c1049bba6c6
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-09-07 10:30:42 +0100

    Modified User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true         
+  has_many :groups, dependent: :destroy
+  has_many :entities, through: :groups, foreign_key: 'author_id'
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
 end
