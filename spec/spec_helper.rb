4a5fc30bd5627bb37f8994c6ed63b7e202c0304d
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-09-08 19:41:49 +0100

    Fixed Linters: Modified Files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,9 +24,9 @@ RSpec.configure do |config|
     #     be_bigger_than(2).and_smaller_than(4).description
     #     # => "be bigger than 2 and smaller than 4"
     # ...rather than:
-    #     # => "be bigger than 2"
-  expectations.include_chain_clauses_in_custom_matcher_descriptions = true
- end
+  #     # => "be bigger than 2"
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
@@ -35,14 +35,14 @@ RSpec.configure do |config|
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
   mocks.verify_partial_doubles = true
-end
+  end
 
 # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
 # have no way to turn it off -- the option exists only for backwards
 # compatibility in RSpec 3). It causes shared context metadata to be
 # inherited by the metadata hash of host groups and examples, rather than
 # triggering implicit auto-inclusion in groups with matching metadata.
-config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.shared_context_metadata_behavior = :apply_to_host_groups
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
